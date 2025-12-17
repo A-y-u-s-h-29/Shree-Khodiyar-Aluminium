b7edce8a3083d7f1eb2c6d135bdbf1db2abd2727
Ayush Sharma <your-email@example.com>
2025-12-17 21:19:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ShreeKhodiyarAluminium from './KhodiyarAluminiumWebsite'
+
+function App() {
+  return (
+    <>
+    <ShreeKhodiyarAluminium />
+    </>
+  )
+}
+
+export default App
